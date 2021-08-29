dce3b30942c007c0300d4812b2ed9a735fb5cc71
andreytsimbalov <andrey.tsimbalov@mail.ru>
2021-08-29 15:51:58 +0300

    db generators and get_currency_value

--- constants.py
+++ constants.py
@@ -1,3 +1,4 @@
 URL_CURRENCIES_LIST = "http://www.cbr.ru/scripts/XML_valFull.asp"
 URL_CURRENCY_DAILY = "https://www.cbr.ru/scripts/XML_daily.asp"
 URL_CURRENCY_DYNAMIC = "https://www.cbr.ru/scripts/XML_dynamic.asp"
+DATABASE = 'currencies.db'
--- test_db.py
+++ test_db.py
@@ -1,18 +1,16 @@
 from sqlalchemy import create_engine, Sequence, Column, Integer, String, Float, DateTime, ForeignKey
 from sqlalchemy.orm import declarative_base, sessionmaker
-# import datetime
+import sqlalchemy
+import datetime as dt
 from datetime import datetime, date
 
 import requests
 import xmltodict
-from flask import Flask, request, jsonify
 from constants import *
 import os
 
-DATABASE = 'currencies.db'
-
-if os.path.exists(DATABASE):
-    os.remove(DATABASE)
+# if os.path.exists(DATABASE):
+#     os.remove(DATABASE)
 engine = create_engine('sqlite:///' + DATABASE, echo=True)
 Base = declarative_base()
 
@@ -71,14 +69,12 @@ def generate_currency_table(url=URL_CURRENCIES_LIST):
         if item['ISO_Char_Code'] != None:
             currency = Currency(item['@ID'], item['ISO_Char_Code'], item['Name'])
             session.add(currency)
+
     session.commit()
 
 
-generate_currency_table()
+# generate_currency_table()
 print(session.query(Currency).all())
-print(session.query(Currency).all()[0].currency_id)
-
-currency = session.query(Currency).all()[0]
 
 
 def add_to_value_table_for_period(
@@ -91,21 +87,74 @@ def add_to_value_table_for_period(
         'date_req2': date_req2,
         'VAL_NM_RQ': cur_code,
     }
-    print(date_req1, date_req2, cur_code)
     response = requests.get(url, params=parameters)
     tree = xmltodict.parse(response.content)
-    for record in tree['ValCurs']['Record']:
+    if 'Record' in tree['ValCurs']:
+        for record in tree['ValCurs']['Record']:
+            cur_date = datetime.strptime(record['@Date'], '%d.%m.%Y')
+            cur_value = record['Value'].replace(',', '.')
+            cur_value = float(cur_value)
+            value = Value(cur_code, cur_date, cur_value)
+            session.add(value)
+        session.commit()
+    else:
+        print(cur_code, 'has not values for period')
 
-        cur_date = datetime.strptime(record['@Date'], '%d.%m.%Y')
-        cur_value = record['Value'].replace(',', '.')
-        cur_value = float(cur_value)
-        value = Value(cur_code, cur_date, cur_value)
-        session.add(value)
-    session.commit()
+
+# add_to_value_table_for_period(currency.currency_id)
+# add_to_value_table_for_period('R01510A')
 
 
-add_to_value_table_for_period(currency.currency_id)
-print(session.query(Value).all()[:5])
+# print(session.query(Value).all()[:5])
+
+def generate_value_table_for_period(
+        date_req1='01/01/2000',
+        date_req2=date.today().strftime("%d/%m/%Y"),
+        url=URL_CURRENCY_DYNAMIC):
+    for currency in session.query(Currency).all():
+        print(currency.currency_id)
+        add_to_value_table_for_period(currency.currency_id, date_req1, date_req2, url)
+
+
+# generate_value_table_for_period()
+# print(len(session.query(Value).all()))
+# print(session.query(Value).all()[:10])
+
+# <Values(1, 'R01010', 2000-01-01 00:00:00, 17.63>,
+# <Values(1, 'R01010', 2000-01-06 00:00:00, 17.63>,
+
+
+def get_currency_value(currency_id, cur_req, url=URL_CURRENCY_DAILY):
+    value = session.query(Value).filter(Value.currency_id == currency_id).filter(Value.date == cur_req).all()
+    if value==[]:
+        print('==find internet')
+        date_req = cur_req.strftime("%d/%m/%Y")
+        parameters = {'date_req': date_req}
+        response = requests.get(url, params=parameters)
+        tree = xmltodict.parse(response.content)
+        valutes = tree['ValCurs']['Valute']
+        valute_value = None
+        for valute in valutes:
+            if valute['@ID'] == currency_id:
+                valute_value = valute['Value'].replace(',', '.')
+                valute_value = float(valute_value)
+
+                value = Value(currency_id, cur_req, valute_value)
+                session.add(value)
+                session.commit()
+                break
+        value = valute_value
+    else:
+        value = value[0].value
+    return value
+
+qwe = get_currency_value('R01010', datetime(2000,1,3))
+
+print(date(2000,1,1))
+qwe = session.query(Value).filter(Value.currency_id == 'R01010').filter(Value.date <= datetime(2000,1,6)).all()
+print(qwe)
+
+
 
 # currency = Currency('3', 'ddd', '123')
 # session.add(currency)
